3a51de6a9ad641615148096f2ffe9a4cd3193138
GusPetito <guspetito@gmail.com>
2020-06-10 17:49:12 -0500

    fixed axes and multiprocessing

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from DataLogger import DataLogger
+from multiprocessing import Process
 
+def start_gui():
+    import GUI
 
-DataLogger("test2.csv")
+def start_data_logging():
+    DataLogger("test2.csv")
+
+p1 = Process(target=start_gui)
+p1.start()
+p2 = Process(target=start_data_logging)
+p2.start()
+p1.join()
+p2.join()
