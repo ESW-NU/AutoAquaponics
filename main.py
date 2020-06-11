ef8aaf512527aeaf0d6de1e26782897ebafdeb4b
GusPetito <guspetito@gmail.com>
2020-06-11 10:23:16 -0500

    possible fix for terminal not closing

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ def start_GUI():
 def start_data_logging():
     DataLogger("test.csv")
 
-p1 = Process(target=start_GUI)
-p1.start()
-p2 = Process(target=start_data_logging)
-p2.start()
-p1.join()
-p2.join()
+if(__name__ == '__main__'):
+    p1 = Process(target=start_GUI)
+    p1.daemon = True
+    p2 = Process(target=start_data_logging)
+    p2.daemon = True
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
