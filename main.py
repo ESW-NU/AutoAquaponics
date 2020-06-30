74772ccc873921214c78efeef861b893dc5bdb39
Bill <66648349+billyen33@users.noreply.github.com>
2020-06-30 16:37:54 -0500

    Billyen33/issue12 (#17)
    * fixed bug with small CSV
    
    * fully fixed short csv bug

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from DataLogger import DataLogger
+from time import sleep
+import csv
+from fakeDataLogger import fakeDataLogger
 from multiprocessing import Process
 global IS_ALIVE
 IS_ALIVE = True
@@ -7,15 +9,27 @@ def start_GUI():
     import GUI
 
 def start_data_logging():
-    DataLogger("test.csv")
+    fakeDataLogger("test2.csv")
 
 if(__name__ == '__main__'):
-    p1 = Process(target=start_GUI)
+    p1 = Process(target=start_data_logging)
     p1.daemon = True
-    p2 = Process(target=start_data_logging)
+    p2 = Process(target=start_GUI)
     p2.daemon = True
-    #start the two processes
+    #start data logging
     p1.start()
+    #create CSV file if it doesn't already exist
+    file_name = "/Users/Bill Yen/Desktop/NU Urban Ag/test2.csv"
+    create_file = open(file_name, "a+")
+    create_file.close()
+    #check length of CSV and wait for data logger to populate if it's too small
+    input_file = open(file_name, "r")
+    reader_file = csv.reader(input_file)
+    dataLen = len(list(reader_file))
+    if dataLen < 2:
+        sleep(10.1)
+    input_file.close()
+    #start GUI
     p2.start()
     p1.join()
     p2.join()
