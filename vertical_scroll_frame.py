f8bd042f03de609b53a2ae26ce7e52cf8a7d1c39
Bill <66648349+billyen33@users.noreply.github.com>
2020-07-11 12:47:24 -0500

    bound scroll bar to mouse scroll

--- vertical_scroll_frame.py
+++ vertical_scroll_frame.py
@@ -1,7 +1,7 @@
 '''
 Code below found from GitHub in {vertical_scroll_frame.py}
 {https://github.com/CS196Illinois/Finance/tree/master/MVP}
-Used to create vertical scroll bar on Dashboard
+Used to create vertical scroll bar on Dashboard, modified for sizing and binding to mouse scroll
 '''
 import tkinter as tk
 
@@ -17,20 +17,20 @@ class VerticalScrolledFrame(tk.Frame):
         # create a canvas object and a vertical scrollbar for scrolling it
         vscrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
         vscrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
-        canvas = tk.Canvas(self, bd=5, highlightthickness=0,bg="white",
+        self.canvas = tk.Canvas(self, bd=5, highlightthickness=0,bg="white",
                         yscrollcommand=vscrollbar.set)
-        canvas.config(width=100, height=580)
-        canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
-        vscrollbar.config(command=canvas.yview)
+        self.canvas.config(width=100, height=580)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
+        vscrollbar.config(command=self.canvas.yview)
 
         # reset the view
-        canvas.xview_moveto(0)
-        canvas.yview_moveto(0)
-        canvas.canvasheight = 580
+        self.canvas.xview_moveto(0)
+        self.canvas.yview_moveto(0)
+        self.canvas.canvasheight = 580
 
         # create a frame inside the canvas which will be scrolled with it
-        self.interior = interior = tk.Frame(canvas)
-        interior_id = canvas.create_window(0, 0, window=interior,
+        self.interior = interior = tk.Frame(self.canvas)
+        interior_id = self.canvas.create_window(0, 0, window=interior,
                                            anchor=tk.NW)
 
         # track changes to the canvas and frame width and sync them,
@@ -38,47 +38,20 @@ class VerticalScrolledFrame(tk.Frame):
         def _configure_interior(event):
             # update the scrollbars to match the size of the inner frame
             size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
-            canvas.config(scrollregion="0 0 %s %s" % size)
-            if interior.winfo_reqwidth() != canvas.winfo_width():
+            self.canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != self.canvas.winfo_width():
                 # update the canvas's width to fit the inner frame
-                canvas.config(width=interior.winfo_reqwidth())
+                self.canvas.config(width=interior.winfo_reqwidth())
 
         interior.bind('<Configure>', _configure_interior)
 
         def _configure_canvas(event):
             if interior.winfo_reqwidth() != canvas.winfo_width():
                 # update the inner frame's width to fill the canvas
-                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
-        canvas.bind('<Configure>', _configure_canvas)
-
-        self.offset_y = 0
-        self.prevy = 0
-        self.scrollposition = 1
-
-        def on_press(event):
-            self.offset_y = event.y_root
-            if self.scrollposition < 1:
-                self.scrollposition = 1
-            elif self.scrollposition > self.canvasheight:
-                self.scrollposition = self.canvasheight
-            canvas.yview_moveto(self.scrollposition / self.canvasheight)
-
-        def on_touch_scroll(event):
-            nowy = event.y_root
-
-            sectionmoved = 15
-            if nowy > self.prevy:
-                event.delta = -sectionmoved
-            elif nowy < self.prevy:
-                event.delta = sectionmoved
-            else:
-                event.delta = 0
-            self.prevy= nowy
-
-            self.scrollposition += event.delta
-            canvas.yview_moveto(self.scrollposition/ self.canvasheight)
-
-        self.bind("<Enter>", lambda _: self.bind_all('<Button-1>', on_press), '+')
-        self.bind("<Leave>", lambda _: self.unbind_all('<Button-1>'), '+')
-        self.bind("<Enter>", lambda _: self.bind_all('<B1-Motion>', on_touch_scroll), '+')
-        self.bind("<Leave>", lambda _: self.unbind_all('<B1-Motion>'), '+')
+                self.canvas.itemconfigure(interior_id, width=self.canvas.winfo_width())
+        self.canvas.bind('<Configure>', _configure_canvas)
+    
+    #this part binds scroll bar to mouse scroll
+        self.bind_all("<MouseWheel>", self._on_mousewheel)
+    def _on_mousewheel(self, event):
+        self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
