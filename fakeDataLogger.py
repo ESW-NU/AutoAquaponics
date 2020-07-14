82f6043ec3c4081c2c265e0943f8aaea41e513e5
mady-c <56701262+mady-c@users.noreply.github.com>
2020-07-14 18:46:03 -0500

    Update fakeDataLogger.py

--- fakeDataLogger.py
+++ fakeDataLogger.py
@@ -34,6 +34,6 @@ def fakeDataLogger(file_name):
                     voltage1_list = voltage_list[-20:]
                     #append data to csv file & plot
                     now = datetime.now()
-                    dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
+                    dt_string = now.strftime("%m/%d/%Y %I:%M:%S %p")
                     writer.writerow([dt_string, voltage, voltage1])
                     file.flush()
