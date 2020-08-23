9d4615dfa7e0e1e12024a926d0f70e7f58da6d52
mady-c <56701262+mady-c@users.noreply.github.com>
2020-08-23 15:31:06 -0500

    Functionized the code

--- sendtext.py
+++ sendtext.py
@@ -1,48 +1,78 @@
 
+
+## this code sends a message to a keyworded dictionary of phone numbers, where the phone number is the key 
+    ## and the carrier is the value. Carriers are case sensitive. Numbers must be in XXXYYYZZZZ format, 
+    ## with no 1 at the beginning and no - in between sections
+
 import smtplib 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-email = "nu.urbanagriculture@gmail.com"
-pas = "Hydroponics1!1"
+numbers = {'2029266579' : "T-Mobile", "2243459408" : "AT&T"} 
+#'8479879824' : "AT&T", 
 
-sms_gateway = '12029266579@tmomail.net' #change this to .. you know, not Mady's number
+def sendtext(message, **numbers):
+    
+    numberlist =[]
+    for number, carrier in numbers.items(): 
+        if carrier == "AT&T":
+            numemail = number+"@txt.att.net"
+            #numemail1 = "1"+number+"@txt.att.net"
+        elif carrier == "Sprint":
+            numemail = number+'@messaging.sprintpcs.com'
+            #numemail1 = "1"+number+'@messaging.sprintpcs.com'   
+        elif carrier == "T-Mobile":
+            numemail = number+'@tmomail.net'
+            #numemail1 = "1"+number+'@tmomail.net'
+        elif carrier == "Verizon":
+            numemail = number+'@vtext.com'
+            #numemail1 = "1"+number+'@vtext.com'
+        elif carrier == "Boost Mobile":
+            numemail = number+'@myboostmobile.com'
+            #numemail1 = "1"+number+'@myboostmobile.com'
+        elif carrier == "Cricket":
+            numemail = number+'@sms.mycricket.com'
+            #numemail1 = "1"+number+'@sms.mycricket.com'
+        elif carrier == "Metro PCS":
+            numemail = number+'@mymetropcs.com'
+            #numemail1 = "1"+number+'@mymetropcs.com'
+        elif carrier == "Tracfone":
+            numemail = number+'@mmst5.tracfone.com'
+            #numemail1 = "1"+number+'@mmst5.tracfone.com'
+        elif carrier == "U.S. Cellular":
+            numemail = number+'@email.uscc.net'
+            #numemail1 = "1"+number+'@email.uscc.net'
+        elif carrier == "Virgin Mobile":
+            numemail = number+'@vmobl.com'
+            #numemail1 = "1"+number+'@vmobl.com'
+        numberlist.append(numemail)
+        #numberlist.append(numemail1)
+    print(numberlist)
 
-# SMS Gateways for each Carrier
-# AT&T: [number]@txt.att.net
-# Sprint: [number]@messaging.sprintpcs.com or [number]@pm .sprint.com
-# T-Mobile: [number]@tmomail.net
-# Verizon: [number]@vtext.com
-# Boost Mobile: [number]@myboostmobile.com
-# Cricket: [number]@sms.mycricket.com
-# Metro PCS: [number]@mymetropcs.com
-# Tracfone: [number]@mmst5.tracfone.com
-# U.S. Cellular: [number]@email.uscc.net
-# Virgin Mobile: [number]@vmobl.com
+    email = "nu.urbanagriculture@gmail.com"
+    pas = "Hydroponics1!1"
 
+    # The server we use to send emails in our case it will be gmail but every email provider has a different smtp 
+    # and port is also provided by the email provider.
+    smtp = "smtp.gmail.com" 
+    port = 587
+    # This will start our email server
+    server = smtplib.SMTP(smtp,port)
+    # Starting the server
+    server.starttls()
+    # Now we need to login
+    server.login(email,pas)
 
-# The server we use to send emails in our case it will be gmail but every email provider has a different smtp 
-# and port is also provided by the email provider.
-smtp = "smtp.gmail.com" 
-port = 587
-# This will start our email server
-server = smtplib.SMTP(smtp,port)
-# Starting the server
-server.starttls()
-# Now we need to login
-server.login(email,pas)
+    for entry in numberlist:
+        msg = MIMEMultipart()
+        sms_gateway = entry
+        msg['From'] = email
+        msg['To'] = sms_gateway
+        body = message
+        msg['Subject'] = "Aquaponics Update\n"
+        msg.attach(MIMEText(body)) #, 'plain'))
+        sms = msg.as_string()
+        
+        server.sendmail(email,sms_gateway,sms)
 
-# Now we use the MIME module to structure our message.
-msg = MIMEMultipart()
-msg['From'] = email
-msg['To'] = sms_gateway
-# Make sure you add a new line in the subject
-msg['Subject'] = "Aquaponics Update\n"
-# Make sure you also add new lines to your body
-body = "your plants are on fire\n"
-# and then attach that body furthermore you can also send html content.
-msg.attach(MIMEText(body)) #, 'plain'))
-
-sms = msg.as_string()
-
-server.sendmail(email,sms_gateway,sms)
+# sendtext("test message  ", **numbers)
