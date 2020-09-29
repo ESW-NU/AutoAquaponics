8f7a0cf3c248652a69d7f62eed39de0801a7a519
Bill <66648349+billyen33@users.noreply.github.com>
2020-09-29 16:57:24 -0500

    added DS18B20 and DHT22 sensors

--- getData.py
+++ getData.py
@@ -7,10 +7,25 @@ def getData():
     import adafruit_ads1x15.ads1115 as ADS
 #import ADS1x15 library's version of AnalogIn
     from adafruit_ads1x15.analog_in import AnalogIn
+#import Adafruit DHT22 stuff (humidty)
+    import Adafruit_DHT as dht
+    DHT = 14 #set DHT's GPIO pin number
+#import the w1 water temp sensor module
+    from w1thermsensor import W1ThermSensor
+    wt_sensor = W1ThermSensor()
 #create ADS object
     ads = ADS.ADS1115(i2c)
     ads.gain = 2/3
 #single ended mode read for pin 0 and 1
     chan = AnalogIn(ads, ADS.P0)
     chan1 = AnalogIn(ads, ADS.P1)
-    return chan.voltage, chan1.voltage
+#read air temp and air humidity
+    hum, atemp = dht.read_retry(dht.DHT22, DHT)
+#read w1 water temp sensor
+    wtemp = wt_sensor.get_temperature()
+    return chan.voltage, hum, atemp, wtemp
+
+#from time import sleep
+#while True:
+#    print(getData())
+#    sleep(2)
