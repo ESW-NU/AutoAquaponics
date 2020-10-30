f663a47f873b48ed1b97e7b0d0b6bbbae20fa573
ESW Aquaponics <nu.urbanagriculture@gmail.com>
2020-10-30 10:59:22 -0500

    added ultrasonic sensor

--- getData.py
+++ getData.py
@@ -13,6 +13,10 @@ def getData():
 #import the w1 water temp sensor module
     from w1thermsensor import W1ThermSensor
     wt_sensor = W1ThermSensor()
+#import libraries for distance sensor
+    import time
+    import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BCM) #GPIO Mode (BOARD / BCM)
 #create ADS object
     ads = ADS.ADS1115(i2c)
     ads.gain = 2/3
@@ -27,10 +31,39 @@ def getData():
     hum, atemp = dht.read_retry(dht.DHT22, DHT)
 #read w1 water temp sensor
     wtemp = wt_sensor.get_temperature()
-    return pH, TDS, hum, atemp, wtemp
+#setup distance sensing stuff
+    GPIO_TRIGGER = 6 #set GPIO Pins
+    GPIO_ECHO = 18
+    GPIO.setup(GPIO_TRIGGER, GPIO.OUT) #set GPIO direction (IN / OUT)
+    GPIO.setup(GPIO_ECHO, GPIO.IN)
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+    
+    return pH, TDS, hum, atemp, wtemp, distance
     #return tuple(pH, TDS, hum, atemp, wtemp) #chan.voltage == ph
 
-#from time import sleep
-#while True:
-#    print(getData())
+from time import sleep
+while True:
+    print(getData())
     sleep(0.2)
