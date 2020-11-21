739409b4208ce375ba74e09e13a50552e522873e
Bill <66648349+billyen33@users.noreply.github.com>
2020-11-21 11:23:24 -0600

    made CP buttoframe background white

--- GUI.py
+++ GUI.py
@@ -611,15 +611,15 @@ class AltControlPanelMain(tk.Frame):
         tk.Label(self, text="Control Panel", bg="white", font=TITLE_FONT).pack(pady = 20)
 
         #Setup for lables and button images
-        self.path_setup = "home/pi/AutoAquaponics" #Change this string to empty if not running on Rpi
+        path_setup = "C:\\Users\\Bill Yen\\Desktop\\NU Urban Ag\\AutoAquaponics\\" #Change this string to empty if not running on Rpi
         self.ctrl_panel_labels = ["Lights", "Water Pump", "Fish Feeder", "Sensor Array", "Oxygenator", "Backwashing", "Fish Camera", "Back"] 
-        self.ctrl_panel_image_path = [path_setup + "Images/light.png", path_setup + "Images/water.png", path_setup + "Images/food.png",  path_setup + "Images/sensor.png", path_setup + "Images/oxygen.png", path_setup + "Images/backwash.png", path_setup + "Images/camera.png", path_setup +"Images/back.png"]
+        self.ctrl_panel_image_path = [path_setup + "Images\\light.png", path_setup + "Images\\water.png", path_setup + "Images\\food.png",  path_setup + "Images\\sensor.png", path_setup + "Images\\oxygen.png", path_setup + "Images\\backwash.png", path_setup + "Images\\camera.png", path_setup +"Images\\back.png"]
         self.ctrl_panel_image = []
 
         for i in range(8):
                 self.ctrl_panel_image.append(tk.PhotoImage(file = self.ctrl_panel_image_path[i])) #create array of images using image path
         
-        buttonFrame = tk.Frame(master=self)
+        buttonFrame = tk.Frame(master=self, bg='white')
         buttonFrame.pack(fill=tk.BOTH, side=tk.BOTTOM, expand=True)
         i = 0
         j = 0
