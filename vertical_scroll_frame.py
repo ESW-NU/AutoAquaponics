69b825f8c4025ae1983bd98a6eb412f1e0fb3590
ESW Aquaponics <nu.urbanagriculture@gmail.com>
2020-12-06 17:42:27 -0600

    fixed scrollbar not bound to mousewheel unless mouse hovers over scrollbar bug

--- vertical_scroll_frame.py
+++ vertical_scroll_frame.py
@@ -4,6 +4,8 @@ Code below found from GitHub in {vertical_scroll_frame.py}
 Used to create vertical scroll bar on Dashboard, modified for sizing and binding to mouse scroll
 '''
 import tkinter as tk
+import functools
+fp = functools.partial
 
 class VerticalScrolledFrame(tk.Frame):
     """A pure Tkinter scrollable frame that actually works!
@@ -12,6 +14,43 @@ class VerticalScrolledFrame(tk.Frame):
     * This frame only allows vertical scrolling
     """
     def __init__(self, parent, *args, **kw):
+        
+        # track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar
+        def _configure_interior(event):
+            # update the scrollbars to match the size of the inner frame
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            self.canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != self.canvas.winfo_width():
+                # update the canvas's width to fit the inner frame
+                self.canvas.config(width=interior.winfo_reqwidth())
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != self.canvas.winfo_width():
+                # update the inner frame's width to fill the canvas
+                self.canvas.itemconfigure(interior_id, width=self.canvas.winfo_width())
+        #this part binds scroll bar to mouse scroll
+        def _on_mousewheel(event, scroll):
+            self.canvas.yview_scroll(int(scroll), "units")
+        def _bind_to_mousewheel(event):
+            self.canvas.bind_all("<Button-4>", fp(_on_mousewheel, scroll=-1))
+            self.canvas.bind_all("<Button-5>", fp(_on_mousewheel, scroll=1))
+        def _unbind_from_mousewheel(event):
+            self.canvas.unbind_all("<Button-4>")
+            self.canvas.unbind_all("<Button-5>")
+            
+        """
+        #This is the Windows code for scrolling the Frame
+        
+        def _on_mousewheel(event):
+            self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+
+        def _bind_to_mousewheel(event):
+            self.canvas.bind_all("<MouseWheel>", _on_mousewheel)
+
+        def _unbind_from_mousewheel(event):
+            self.canvas.unbind_all("<MouseWheel>")
+        """
+        
         tk.Frame.__init__(self, parent, *args, **kw)
 
         # create a canvas object and a vertical scrollbar for scrolling it
@@ -32,28 +71,8 @@ class VerticalScrolledFrame(tk.Frame):
         self.interior = interior = tk.Frame(self.canvas)
         interior_id = self.canvas.create_window(0, 0, window=interior,
                                            anchor=tk.NW)
-
-        # track changes to the canvas and frame width and sync them,
-        # also updating the scrollbar
-        def _configure_interior(event):
-            # update the scrollbars to match the size of the inner frame
-            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
-            self.canvas.config(scrollregion="0 0 %s %s" % size)
-            if interior.winfo_reqwidth() != self.canvas.winfo_width():
-                # update the canvas's width to fit the inner frame
-                self.canvas.config(width=interior.winfo_reqwidth())
-
+  
         interior.bind('<Configure>', _configure_interior)
-
-        def _configure_canvas(event):
-            if interior.winfo_reqwidth() != self.canvas.winfo_width():
-                # update the inner frame's width to fill the canvas
-                self.canvas.itemconfigure(interior_id, width=self.canvas.winfo_width())
         self.canvas.bind('<Configure>', _configure_canvas)
-    
-    #this part binds scroll bar to mouse scroll
-        self.bind_all("<MouseWheel>", self._on_mousewheel)
-        self.bind_all("<Button-4>", self._on_mousewheel)
-        self.bind_all("<Button-5>", self._on_mousewheel)
-    def _on_mousewheel(self, event):
-        self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+        self.canvas.bind('<Enter>', _bind_to_mousewheel)
+        self.canvas.bind('<Leave>', _unbind_from_mousewheel)
