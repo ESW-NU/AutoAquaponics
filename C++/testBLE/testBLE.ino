aa5d81b96e922f6364e6c46f5f049022ea4e1f4b
billyen33 <billmembers33@gmail.com>
2021-04-29 17:10:19 -0500

    Johnny's code

--- C++/testBLE/testBLE.ino
+++ C++/testBLE/testBLE.ino
@@ -2,79 +2,70 @@
     Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleWrite.cpp
     Ported to Arduino ESP32 by Evandro Copercini
 */
-
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
-
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
-
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-long int freq = 1000; //default LED blinking frequency
-char temp_freq;
-int ledPin = 2; //led pin number
-
+uint16_t freq;
+int ledState = LOW;
+long previousMillis = 0;
+int ledPin = 2; 
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string value = pCharacteristic->getValue();
-      
       if (value.length() > 0) {
         Serial.println("*********");
         Serial.print("New value: ");
-        temp_freq = value[0];
-        for (int i = 1; i < value.length(); i++){
-          temp_freq = temp_freq + value[i];
-          Serial.print(value[i]); //actual correct value (minus the first digit)
+        freq = 0;
+        for (int i = 0; i < value.length(); i++) {
+          freq <<= 8;
+          freq |= value[i]; 
         }
-        Serial.println();
-        freq = (long int) temp_freq;
-        Serial.print(freq); //the actual frequency value (wrong!)
-        Serial.println();
-        Serial.print(temp_freq); //the string version of the freq value (in ASCII as char, not number, weird)
+        Serial.print(freq);
         Serial.println();
         Serial.println("*********");
       }
     }
 };
-
 void setup() {
-  // Set LED as output
   pinMode(ledPin, OUTPUT);
   Serial.begin(115200);
-
   Serial.println("1- Download and install an BLE scanner app in your phone");
   Serial.println("2- Scan for BLE devices in the app");
   Serial.println("3- Connect to MyESP32");
   Serial.println("4- Go to CUSTOM CHARACTERISTIC in CUSTOM SERVICE and write something");
   Serial.println("5- See the magic =)");
-
   BLEDevice::init("MyESP32");
   BLEServer *pServer = BLEDevice::createServer();
-
   BLEService *pService = pServer->createService(SERVICE_UUID);
-
   BLECharacteristic *pCharacteristic = pService->createCharacteristic(
                                          CHARACTERISTIC_UUID,
                                          BLECharacteristic::PROPERTY_READ |
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
-
   pCharacteristic->setCallbacks(new MyCallbacks());
-
-  pCharacteristic->setValue("Hello World");
+  pCharacteristic->setValue("Hello WOrld");
   pService->start();
-
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
   pAdvertising->start();
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(ledPin, HIGH);
-  delay(freq);
-  digitalWrite(ledPin, LOW);  
-  delay(freq);
-  //delay(2000);
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis > freq) {
+    // save the last time you blinked the LED 
+    previousMillis = currentMillis;   
+ 
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW)
+      ledState = HIGH;
+    else
+      ledState = LOW;
+ 
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+  }
 }
