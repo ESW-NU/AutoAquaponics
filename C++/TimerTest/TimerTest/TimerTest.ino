42c50169b5a0a33f77ec84df2313e16b3bd1ad8d
Johnny <johnnychen2023@u.northwestern.edu>
2021-05-18 14:46:25 -0500

    Testing struct containing state and timer

---
+++ C++/TimerTest/TimerTest/TimerTest.ino
@@ -0,0 +1,99 @@
+/*
+ Repeat timer example
+
+ This example shows how to use hardware timer in ESP32. The timer calls onTimer
+ function every second. The timer can be stopped with button attached to PIN 0
+ (IO0).
+
+ This example code is in the public domain.
+ */
+
+// Stop button is attached to PIN 0 (IO0)
+#define BTN_STOP_ALARM    0
+#define MAX_ALARMS 500
+
+typedef struct virt_alarm {
+  uint32_t time_hit;
+  uint16_t state;
+} virt_alarm;
+
+hw_timer_t * timer = NULL;
+volatile SemaphoreHandle_t timerSemaphore;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+virt_alarm al_list[500];
+uint32_t current_list = 0;
+
+volatile uint32_t isrCounter = 0;
+volatile uint32_t lastIsrAt = 0;
+
+void IRAM_ATTR onTimer(){
+  // Increment the counter and set the time of ISR
+  portENTER_CRITICAL_ISR(&timerMux);
+  isrCounter++;
+  lastIsrAt = millis();
+  portEXIT_CRITICAL_ISR(&timerMux);
+  // Give a semaphore that we can check in the loop
+  xSemaphoreGiveFromISR(timerSemaphore, NULL);
+  if (current_list < MAX_ALARMS) {
+    current_list++;
+    timerAlarmWrite(timer, al_list[current_list].time_hit, false);
+    timerAlarmEnable(timer);
+  }
+  // It is safe to use digitalRead/Write here if you want to toggle an output
+}
+
+void setup() {
+  Serial.begin(115200);
+  al_list[0] = virt_alarm {10000, 10};
+  al_list[1] = virt_alarm {30000, 10};
+  al_list[2] = virt_alarm {40000, 10};
+  al_list[3] = virt_alarm {50000, 10};
+
+  // Set BTN_STOP_ALARM to input mode
+  pinMode(BTN_STOP_ALARM, INPUT);
+
+  // Create semaphore to inform us when the timer has fired
+  timerSemaphore = xSemaphoreCreateBinary();
+
+  // Use 1st timer of 4 (counted from zero).
+  // Set 80 divider for prescaler (see ESP32 Technical Reference Manual for more
+  // info).
+  timer = timerBegin(0, 8000, true);
+
+  // Attach onTimer function to our timer.
+  timerAttachInterrupt(timer, &onTimer, true);
+
+  // Set alarm to call onTimer function every second (value in microseconds).
+  // Repeat the alarm (third parameter)
+  timerAlarmWrite(timer, al_list[0].time_hit, false);
+
+  // Start an alarm
+  timerAlarmEnable(timer);
+}
+
+void loop() {
+  // If Timer has fired
+  if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE){
+    uint32_t isrCount = 0, isrTime = 0;
+    // Read the interrupt count and time
+    portENTER_CRITICAL(&timerMux);
+    isrCount = isrCounter;
+    isrTime = lastIsrAt;
+    portEXIT_CRITICAL(&timerMux);
+    // Print it
+    Serial.print("onTimer no. ");
+    Serial.print(isrCount);
+    Serial.print(" at ");
+    Serial.print(isrTime);
+    Serial.println(" ms");
+  }
+  // If button is pressed
+  if (digitalRead(BTN_STOP_ALARM) == LOW) {
+    // If timer is still running
+    if (timer) {
+      // Stop and free timer
+      timerEnd(timer);
+      timer = NULL;
+    }
+  }
+}
