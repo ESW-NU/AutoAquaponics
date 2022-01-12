ba28bddc2912c0e10d541c01ef7635c0f51919e8
larinachen <larinachen2024@u.northwestern.edu>
2022-01-11 20:05:54 -0600

    commit

--- GUI.py
+++ GUI.py
@@ -809,7 +809,9 @@ class Lights(tk.Frame):
             elif i == 3:
                 self.togBask.config(text="ON", fg="green")
             # send message
-            ble.BLE_messenger(1,1) # 1 is on, outlet 1
+            msg = ble.BLE_messenger(1,1) # 1 is on, outlet 1
+            # prints message
+            ble.BLE_write('1',msg)
         elif lights_config[i] == "on":
             lights_config[i] = "timer"
             if i == 0:
@@ -820,7 +822,10 @@ class Lights(tk.Frame):
                 self.togTank.config(text="TIMER", fg="purple")
             elif i == 3:
                 self.togBask.config(text="TIMER", fg="purple")
-            ble.BLE_messenger(2,1) # 2 is timer mode, outlet 1
+            # send message
+            msg = ble.BLE_messenger(2,1) # 2 is timer mode, outlet 1
+            # prints message
+            ble.BLE_write('1',msg)
         elif lights_config[i] == "timer":
             lights_config[i] = "off"
             if i == 0:
@@ -831,7 +836,10 @@ class Lights(tk.Frame):
                 self.togTank.config(text="OFF", fg="red")
             elif i == 3:
                 self.togBask.config(text="OFF", fg="red")
-            ble.BLE_messenger(0,1) # 0 is off, outlet 1
+            # send message
+            msg = ble.BLE_messenger(0,1) # 0 is off, outlet 1
+            # prints message
+            ble.BLE_write('1',msg)
         csv_write('lights_config', lights_config)
         #ble.BLE_message(0b10, )
         #ble.BLE_write('0', 50) #0 is the outlet box, make the message dependent on which button is pressed (not just 50)
