aad9b5cf07077d0e09e080abd7e9b71d38e477c8
billyen33 <billmembers33@gmail.com>
2022-07-09 15:57:02 -0500

    fixed hardcoded outlet box bug

--- C++/hardcoded_outlet_timer/hardcoded_outlet_timer.ino
+++ C++/hardcoded_outlet_timer/hardcoded_outlet_timer.ino
@@ -1,24 +1,32 @@
 #include "interrupt.h"
 
 void setup() {
+  //initialize interrupt
+  init_interrupt();
   // put your setup code here, to run once:
+  pinMode(VALVE_1, OUTPUT); //screw terminal 1
+  pinMode(VALVE_2, OUTPUT); //screw terminal 2
   pinMode(UPPER_GLIGHT, OUTPUT); //output 1
   pinMode(LOWER_GLIGHT, OUTPUT); //output 2
   pinMode(FISH_LIGHT, OUTPUT); //output 3
+  pinMode(PUMP, OUTPUT);//pump
+  digitalWrite(PUMP, LOW); //always keep pump on
+  digitalWrite(VALVE_1, HIGH);
+  digitalWrite(VALVE_2, LOW);
 }
 
 void loop() {
   // turn on all lights
-  digitalWrite(UPPER_GLIGHT, HIGH);
-  digitalWrite(LOWER_GLIGHT, HIGH);
-  digitalWrite(FISH_LIGHT, HIGH);
-  delay(fish_light_dur * 1000);
-
-  //turn off fish light
-  digitalWrite(FISH_LIGHT, LOW);
-  delay((upper_glight_dur-fish_light_dur) * 1000);
-  //turn off plant lights 
   digitalWrite(UPPER_GLIGHT, LOW);
   digitalWrite(LOWER_GLIGHT, LOW);
-  delay((24-upper_glight_dur) * 1000);
+  digitalWrite(FISH_LIGHT, LOW);
+  delay(fish_light_dur * 60 * 60 * 1000);
+
+  //turn off fish light
+  digitalWrite(FISH_LIGHT, HIGH);
+  delay((upper_glight_dur-fish_light_dur) * 60* 60* 1000);
+  //turn off plant lights 
+  digitalWrite(UPPER_GLIGHT, HIGH);
+  digitalWrite(LOWER_GLIGHT, HIGH);
+  delay((24-upper_glight_dur) * 60 * 60 * 1000);
 }
