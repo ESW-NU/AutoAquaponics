e9a0dd31c423c2b13bb7ece2d5baec2f9a73579a
KongFizZzy <landauniv@gmail.com>
2023-02-21 19:47:23 -0600

    small update

--- C++/ESPOutlets/Niv_s_Timer_Tester/Niv_s_Timer_Tester.ino
+++ C++/ESPOutlets/Niv_s_Timer_Tester/Niv_s_Timer_Tester.ino
@@ -28,7 +28,7 @@
 #include <string> 
 #include <iostream> 
 #include <stdint.h>
-const outlet_pins[16] = {O_1, O_2, O_3, O_4, O_5, O_6, O_7, O_8, O_9, O_10, T_1, T_2, T_3, T_4, T_5, T_6}; 
+const int outlet_pins[16] = {O_1, O_2, O_3, O_4, O_5, O_6, O_7, O_8, O_9, O_10, T_1, T_2, T_3, T_4, T_5, T_6}; 
 
 hw_timer_t * timer = NULL;
 volatile SemaphoreHandle_t timerSemaphore;
@@ -73,8 +73,8 @@ void IRAM_ATTR on_min(){
   // will check the mode for all outlets and change on/off and duration values accordingly
   // Needs to be in on_min interrupt because time_cycle values will change too quickly otherwise
   for (int i = 0; i < 16; i++) {
-      mode = outlet_mode[i];
-      swtich(mode){
+      int mode = outlet_mode[i];
+      switch(mode){
         case 0:
         break;
 
@@ -143,7 +143,7 @@ void loop() {
   // the AutoOutlet C++ Code Documentation on Google Drive: 
   // https://docs.google.com/document/d/17H-WvJsHd-YGuLblgH95uoM-LP0ZRlJ7i7fLkkYbfUw/edit
   
-  uint32_t message;                             // needs to be assigned to BLE message from RPi, then if statement if message has been changed. If it has, run switch cases below.
+  uint32_t message  = 0b00000000010000000000000000000011;                             // needs to be assigned to BLE message from RPi, then if statement if message has been changed. If it has, run switch cases below.
   uint32_t req_mode = (message & 0x3);
   uint32_t blue_bits = (message >> 2) & 0xFF;
   uint32_t red_bits = (message >> 10) & 0x7FF;
@@ -154,37 +154,43 @@ void loop() {
   // will alwaye
   switch (req_mode) {
     case 0:                                   // Initialization Mode (00)
-      time = red_bits;                        // Message from RPi showing number of ten-minute intervals from start of day
+    {
+      uint32_t time = red_bits;               // Message from RPi showing number of ten-minute intervals from start of day
       int hrs = time/60;                      // Number of hours from start of the day
       int tens = (time % 60) / 10;            // Number of ten minute intervals that have passed in given hour
       portENTER_CRITICAL(&timerMux);
       tens_counter = tens;
       hour_counter = hrs;                     // Counter values will be reset here based on time from RPi
       portEXIT_CRITICAL(&timerMux);
+    }
       break;
 
     case 1:                                   // Time Cycle Mode (01) (mode 1)
+    {
       uint32_t outlet_num = blue_bits;
       outlet_mode[outlet_num] = req_mode;
       duration[outlet_num] = red_bits;
+    }
       break;
 
     case 2:                                   // Daily Repeat Mode (10) (mode 2)
+    {
       uint32_t outlet_num = blue_bits;
       outlet_mode[outlet_num] = req_mode;
       uint32_t time_to_turn_on = red_bits;
       on_time[outlet_num] = time_to_turn_on;
       uint32_t duration_10 = green_bits;      // Gets duration in increments of 10 (i.e. value of 4 = 40 minutes)
       duration[outlet_num] = duration_10;
+    }
       break;
 
-    case 3:                                   // Permanent State Mode (11) (mode 3)      
+    case 3:                                   // Permanent State Mode (11) (mode 3)     
+    { 
       uint32_t outlet_num = blue_bits;
       outlet_mode[outlet_num] = req_mode;
       uint32_t state = green_bits;
       on_or_off[outlet_num] = state;
-      uint32_t permenant_or_not = red_bits;
-      //duration[outlet_num] = INT_FAST#@_MAX; //use infinity to show we are in permanant mode
+    }
       break;      
   }
 
@@ -195,7 +201,7 @@ void loop() {
           digitalWrite(on_or_off[i], LOW);          // Turns pin off if on_or_off[i] value is 0
           break;
         case 1:
-          digitalWrite(on_or_off[i], HIGH)          // Turns pin on if on_or_off[i] value is 1
+          digitalWrite(on_or_off[i], HIGH);          // Turns pin on if on_or_off[i] value is 1
           break;          
       }
   }
