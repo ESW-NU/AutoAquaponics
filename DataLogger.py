b4d5b99a56cc90ad8ff571932de85b82cb895d89
Ben Caterine <benrcaterine@gmail.com>
2023-05-02 19:26:05 -0500

    init app

--- DataLogger.py
+++ DataLogger.py
@@ -51,7 +51,7 @@ from firebase_admin import credentials, firestore
 # from firebase.database import onValue
 
 cred = firebase_admin.credentials.Certificate("./serviceAccountKey.json")
-# app = firebase_admin.initialize_app(cred)
+app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 ref = db.collection(u'tolerances').document(u'pH')
 
