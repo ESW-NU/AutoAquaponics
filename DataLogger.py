01eafcc0139dbfc289ce0e43b35157fd93236d10
Ben Caterine <benrcaterine@gmail.com>
2023-05-07 14:30:27 -0500

    new DataLogger

--- DataLogger.py
+++ DataLogger.py
@@ -1,8 +1,29 @@
-from main import user_settings
 from getData import getData
+import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import numpy as np
 
-config_path, db_path, img_path = user_settings()
-db_name = 'sensor_db.db'
+all_we_got_now = ('unix_time', 'pH', 'TDS', 'humidity', 'air_temp', 'water_temp', 'distance')
+cred = credentials.Certificate("./serviceAccountKey.json")
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+print(db)
+LOG_EVERY = 15
+
+ref = db.collection(u'tolerances').document(u'pH')
+
+def on_snapshot(doc_snapshot, changes, read_time):
+    for doc in doc_snapshot:
+        docDict = doc.to_dict()
+        print(docDict)
+        
+doc_watch = ref.on_snapshot(on_snapshot)
+
+print(db)
+LOG_EVERY = 15
 
 param_list = ['pH', 'TDS (ppm)', 'Rela. Humidity (%)', 'Air Temp (\N{DEGREE SIGN}C)', 'Water Temp (\N{DEGREE SIGN}C)', 'Water Level (cm)']
 all_we_got_now = ('unix_time', 'pH', 'TDS', 'humidity', 'air_temp', 'water_temp', 'distance')
@@ -22,23 +43,31 @@ def data_fxn(last_distance, last_wtemp):
     return pH, TDS, hum, atemp, wtemp, distance #N-tuple of random ints from 0 to 10.
 
 def DataLogger():
-    from data import Reader, Logger
     #from getData import getData #comment out if you are testing on computer
-    import numpy as np
-    sensor_plot_table = {'SensorData':(all_we_got_now, now_data_types)}
-    logger = Logger(db_path, db_name)
-    logger.table(sensor_plot_table)
-    last_distance = np.nan #to give an arbitrary initial value to getData for the first time the distance sensor fails
-    last_wtemp = 21 #arbitrary initial value
-    last_hum = np.nan
-    last_atemp = np.nan
+    distance = np.nan #to give an arbitrary initial value to getData for the first time the distance sensor fails
+    wtemp = 21 #arbitrary initial value
+    hum = np.nan
+    atemp = np.nan
+    time_to_log = round(time.time())
+    
     while True:
         #change tsamp and nsamp for logging time/frequency
         #use this for RPi with real sensors (comment line below if on computer, also comment line 2)
-        last_distance, last_wtemp, last_atemp, last_hum = np.round(logger.collect_data("SensorData", getData(last_distance, last_wtemp, last_hum, last_atemp), last_distance, last_wtemp, last_hum, last_atemp, tsamp=1, nsamp=10),2)
+        # last_distance, last_wtemp, last_atemp, last_hum = np.round(logger.collect_data("SensorData", getData(last_distance, last_wtemp, last_hum, last_atemp), tsamp=1, nsamp=10),2)
+        pH, TDS, hum, atemp, wtemp, distance = np.round(getData(distance, wtemp, hum, atemp), 2)
+        curr_time = round(time.time())
+        if curr_time < time_to_log:
+            continue
+        data_tuple = (curr_time, pH, TDS, hum, atemp, wtemp, distance)
         #use this to simulate sensor logging data on computer (comment line below if on RPi)
         #last_distance, last_wtemp, last_atemp, last_hum = np.round(logger.collect_data("SensorData", data_fxn(last_distance, last_wtemp), last_distance, last_wtemp, last_hum, last_atemp, tsamp=1, nsamp=5),2)
-        logger.log_data()
-        logger.commit()
+        # logger.log_data()
+        # logger.commit()
+
+        data_dict = {}
+        for i in range(len(data_tuple)):
+            data_dict[all_we_got_now[i]] = data_tuple[i]
+        db.collection(u'stats').add(data_dict)
+        time_to_log = curr_time + LOG_EVERY * 60
        
 DataLogger()
