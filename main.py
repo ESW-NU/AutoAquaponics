5f485c6ca4a1660b16f115ba10b2c6e8cf0883cc
Ben Caterine <benrcaterine@gmail.com>
2023-05-23 19:01:39 -0500

    update gitignore, main

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # main.py
-from get_data import get_data
+import argparse
+import numpy as np
 import time
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-import numpy as np
+
 from ble import BLE
 from ble import FakeBLE
-import argparse
+from get_data import get_data
 
 parser = argparse.ArgumentParser(description="This script can be run with the following arguments:",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -27,7 +29,7 @@ time.sleep(2)
 ble = BLE() if REAL_BLE else FakeBLE()
 ble.BLE_init()
 
-all_we_got_now = ('unix_time', 'pH', 'TDS', 'humidity', 'air_temp', 'water_temp', 'distance')
+sensors = ('unix_time', 'pH', 'TDS', 'humidity', 'air_temp', 'water_temp', 'distance')
 cred = credentials.Certificate("./serviceAccountKey.json")
 app = firebase_admin.initialize_app(cred)
 
@@ -89,8 +91,8 @@ def find_next_log_time(x, base):
     return maybe
 
 def DataLogger():
-    distance = np.nan #to give an arbitrary initial value to getData for the first time the distance sensor fails
-    wtemp = 21 #arbitrary initial value
+    distance = np.nan  #to give an arbitrary initial value to getData for the first time the distance sensor fails
+    wtemp = 21  #arbitrary initial value
     hum = np.nan
     atemp = np.nan
     curr_time = round(time.time())
@@ -101,12 +103,12 @@ def DataLogger():
         curr_time = round(time.time())
         if curr_time <= time_to_log:
             continue
-        data_tuple = (curr_time, pH, TDS, hum, atemp, wtemp, distance)
+        curr_data = (curr_time, pH, TDS, hum, atemp, wtemp, distance)
         
-        data_dict = {}
-        for i in range(len(data_tuple)):
-            data_dict[all_we_got_now[i]] = data_tuple[i]
-        db.collection(u'stats').add(data_dict)
+        data_as_dict = {}
+        for i in range(len(curr_data)):
+            data_as_dict[sensors[i]] = curr_data[i]
+        db.collection(u'stats').add(data_as_dict)
         time_to_log = find_next_log_time(curr_time, LOG_EVERY * 60)
        
 DataLogger()
